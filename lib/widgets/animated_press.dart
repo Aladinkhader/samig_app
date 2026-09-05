552173ba7fe66ec95d1ff612884746431ef7a784
Aladinkhader <95710135+Aladinkhader@users.noreply.github.com>
2026-09-05 10:20:45 +0200

    Create animated_press.dart

---
+++ lib/widgets/animated_press.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+
+class AnimatedPress extends StatefulWidget {
+  final Widget child;
+  final VoidCallback? onTap;
+  final double scale;
+
+  const AnimatedPress({
+    super.key,
+    required this.child,
+    this.onTap,
+    this.scale = 0.97,
+  });
+
+  @override
+  State<AnimatedPress> createState() => _AnimatedPressState();
+}
+
+class _AnimatedPressState extends State<AnimatedPress> {
+  bool _pressed = false;
+
+  void _setPressed(bool value) {
+    if (!mounted) return;
+
+    setState(() {
+      _pressed = value;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: widget.onTap,
+      onTapDown: (_) => _setPressed(true),
+      onTapUp: (_) => _setPressed(false),
+      onTapCancel: () => _setPressed(false),
+      child: AnimatedScale(
+        scale: _pressed ? widget.scale : 1.0,
+        duration: const Duration(milliseconds: 120),
+        curve: Curves.easeOut,
+        child: widget.child,
+      ),
+    );
+  }
+}
