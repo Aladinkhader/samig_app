90e0cb59b3d325e1a895d070b1ed777b516568fc
Aladinkhader <95710135+Aladinkhader@users.noreply.github.com>
2026-09-05 10:15:09 +0200

    Create AppItem model with required fields

---
+++ lib/models/app_item.dart
@@ -0,0 +1,13 @@
+class AppItem {
+  final String title;
+  final String subtitle;
+  final String icon;
+  final String category;
+
+  const AppItem({
+    required this.title,
+    required this.subtitle,
+    required this.icon,
+    required this.category,
+  });
+}
